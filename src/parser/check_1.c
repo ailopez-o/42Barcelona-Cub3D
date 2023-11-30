37bceeed0d0da959c49d59f9cd745d25ed3b59cc
Ramix76 <framos-p@car8s4.42barcelona.com>
2023-11-30 13:12:13 +0100

    fixed header in src/ml_window/key_events_utils.c and a bit norm for checks.c

--- src/parser/check_1.c
+++ src/parser/check_1.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   validator.c                                        :+:      :+:    :+:   */
+/*   check_1.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bmoll-pe <bmoll-pe@student.42.fr>          +#+  +:+       +#+        */
+/*   By: framos-p <framos-p@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/19 19:02:19 by bmoll-pe          #+#    #+#             */
-/*   Updated: 2023/07/19 19:10:01 by bmoll-pe         ###   ########.fr       */
+/*   Created: 2023/11/30 11:55:26 by framos-p          #+#    #+#             */
+/*   Updated: 2023/11/30 12:07:02 by framos-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,75 +17,82 @@
 #include "mlx.h"
 #include <fcntl.h>
 
-bool validate_map(char *path, t_cub *cub);
-char *get_int_array(char *line);
-int get_data_type(char *line);
-int add_texture(char *path, t_texture *textures, t_mlx *screen, int type);
-int color_parser(char *line);
-int check_map(t_map *map);
-int map_builder(char **int_map, int scale, t_map *map, t_player *player);
-int **resize_matrix(int **matrix, int *width);
-int **get_image_matrix(char *data, int width, int height);
-t_texture *get_texture(t_texture *textures, int type);
-bool valid_map_from_player(int x, int y, char **map, int max_x, int max_y);
-int print_map(char **map);
+bool		validate_map(char *path, t_cub *cub);
+char		*get_int_array(char *line);
+int			get_data_type(char *line);
+int			add_texture(char *path, t_texture *textures, t_mlx *screen,
+				int type);
+int			color_parser(char *line);
+int			check_map(t_map *map);
+int			map_builder(char **int_map, int scale, t_map *map,
+				t_player *player);
+int			**resize_matrix(int **matrix, int *width);
+int			**get_image_matrix(char *data, int width, int height);
+t_texture	*get_texture(t_texture *textures, int type);
+bool		valid_map_from_player(int x, int y, char **map, int max_x,
+				int max_y);
+int			print_map(char **map);
 
-bool parse_map(int argv, char **argc, t_cub *cub)
+bool	parse_map(int argv, char **argc, t_cub *cub)
 {
 	if (argv != 2)
-		return EXIT_FAILURE;
-	if (ft_strlen(argc[1]) < 4 || ft_strncmp(argc[1] + (ft_strlen(argc[1]) - 4), ".cub", 5))
-		return EXIT_FAILURE;
+		return (EXIT_FAILURE);
+	if (ft_strlen(argc[1]) < 4 || ft_strncmp(argc[1] + (ft_strlen(argc[1]) - 4),
+			".cub", 5))
+		return (EXIT_FAILURE);
 	if (validate_map(argc[1], cub))
-		return EXIT_FAILURE;
-	return EXIT_SUCCESS;
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
 }
 
-int is_player(char *line)
+int	is_player(char *line)
 {
-	int pos;
+	int	pos;
 
 	pos = 0;
 	while (line[pos])
 	{
 		if (line[pos] == 'N')
-			return pos;
+			return (pos);
 		if (line[pos] == 'S')
-			return pos;
+			return (pos);
 		if (line[pos] == 'E')
-			return pos;
+			return (pos);
 		if (line[pos] == 'W')
-			return pos;
+			return (pos);
 		pos++;
 	}
-	return 0;
+	return (0);
 }
 
-bool square_map(char **map, int max_x)
+bool	square_map(char **map, int max_x)
 {
-	int i;
-	int len;
+	int	i;
+	int	len;
+	int	j;
 
 	i = 0;
 	while (map[i] != NULL)
 	{
 		len = ft_strlen(map[i]);
 		map[i] = ft_realloc(map[i], max_x + 1);
-		for (int j = len; j < max_x; j++)
+		j = len;
+		while (j < max_x)
+		{
 			map[i][j] = '0';
+			j++;
+		}
 		map[i][max_x] = '\0';
 		i++;
 	}
-
-	return true;
+	return (true);
 }
 
-bool line_has_wall(char *line)
+bool	line_has_wall(char *line)
 {
-	bool has_wall;
+	bool	has_wall;
 
 	has_wall = false;
-
 	if (*line == ' ' || *line == '0')
 	{
 		while (*line)
@@ -95,21 +102,20 @@ bool line_has_wall(char *line)
 			line++;
 		}
 	}
-	return has_wall;
+	return (has_wall);
 }
 
-bool validate_map(char *path, t_cub *cub)
+bool	validate_map(char *path, t_cub *cub)
 {
-	int fd;
-	char *line;
-	char **map;
-	int num_line;
-	int num_textures;
-	int data_type;
-	bool map_parsing;
+	int		fd;
+	char	*line;
+	char	**map;
+	int		num_line;
+	int		num_textures;
+	int		data_type;
+	bool	map_parsing;
 
 	init_cub(cub);
-
 	fd = open(path, O_RDONLY);
 	if (fd < 0)
 		return (EXIT_FAILURE);
@@ -122,7 +128,7 @@ bool validate_map(char *path, t_cub *cub)
 	{
 		if (line[0] && (line[0] == '0' || line[0] == '1' || line[0] == ' '))
 		{
-			if(map_parsing == true || line_has_wall(line) == true)
+			if (map_parsing == true || line_has_wall(line) == true)
 			{
 				map_parsing = true;
 				cub->map.max_y += 1;
@@ -130,7 +136,6 @@ bool validate_map(char *path, t_cub *cub)
 					cub->map.max_x = ft_strlen(line);
 				map[num_line] = get_int_array(line);
 				num_line++;
-
 				if (is_player(line))
 				{
 					cub->player.matrix_pos.x = is_player(line);
@@ -144,12 +149,14 @@ bool validate_map(char *path, t_cub *cub)
 			if (map_parsing == true)
 			{
 				free(line);
-				break;
+				break ;
 			}
 			data_type = get_data_type(line);
-			if (data_type == NO || data_type == SO || data_type == WE || data_type == EA)
+			if (data_type == NO || data_type == SO || data_type == WE
+				|| data_type == EA)
 			{
-				if (add_texture(line, cub->map.textures, &cub->screen, data_type) == EXIT_FAILURE)
+				if (add_texture(line, cub->map.textures, &cub->screen,
+						data_type) == EXIT_FAILURE)
 				{
 					free(line);
 					return (EXIT_FAILURE);
@@ -167,64 +174,65 @@ bool validate_map(char *path, t_cub *cub)
 	square_map(map, cub->map.max_x);
 	if (check_map(&cub->map))
 		return (EXIT_FAILURE);
-	if (valid_map_from_player((int)cub->player.matrix_pos.x, (int)cub->player.matrix_pos.y, map, cub->map.max_x, cub->map.max_y))
+	if (valid_map_from_player((int)cub->player.matrix_pos.x,
+			(int)cub->player.matrix_pos.y, map, cub->map.max_x,
+			cub->map.max_y))
 	{
 		map_builder(map, MAPSCALE, &cub->map, &cub->player);
 		return (EXIT_SUCCESS);
-	}	
+	}
 	else
 		return (EXIT_FAILURE);
 }
 
-void dfs(int x, int y, int ancho, int alto, char **map, bool *encerrado, char **visitado)
+void	dfs(int x, int y, int ancho, int alto, char **map, bool *encerrado,
+			char **visitado)
 {
-	// Si llegamos a un borde, entonces hay una salida
+	int	i;
+	int	nx;
+	int	ny;
+
 	if (x <= 0 || y <= 0 || x >= (ancho - 1) || y >= (alto - 1))
 	{
 		*encerrado = false;
-		return;
+		return ;
 	}
-
-	// Marcar la celda actual como visitada
 	visitado[y][x] = true;
-
 	ancho = ft_strlen(map[y]);
-
-	// Movimientos: arriba, abajo, izquierda, derecha
-	int dx[] = {0, 0, -1, 1};
-	int dy[] = {-1, 1, 0, 0};
-
-	for (int i = 0; i < 4; i++)
+	int	dx[] = {0, 0, -1, 1};
+	int	dy[] = {-1, 1, 0, 0};
+	i = -1;
+	while (++i < 4)
 	{
-		int nx = x + dx[i];
-		int ny = y + dy[i];
-
-		// Verificar si la nueva posición es válida y no ha sido visitada
-		if (nx >= 0 && ny >= 0 && nx <= ancho && ny <= alto && !visitado[ny][nx] && map[ny][nx] == '0')
-		{
+		nx = x + dx[i];
+		ny = y + dy[i];
+		if (nx >= 0 && ny >= 0 && nx <= ancho && ny <= alto && !visitado[ny][nx]
+				&& map[ny][nx] == '0')
 			dfs(nx, ny, ancho, alto, map, encerrado, visitado);
-		}
 	}
 }
 
-bool valid_map_from_player(int x, int y, char **map, int width, int height)
+bool	valid_map_from_player(int x, int y, char **map, int width, int height)
 {
-	char **visited;
-	
+	char	**visited;
+	int		i;
+	int		j;
+	bool	closed;
+
 	visited = ft_calloc(sizeof(bool *), height);
-	for (int i = 0; i < height; i++)
+	i = -1;
+	while (++i < height)
 	{
 		visited[i] = ft_calloc(sizeof(bool), width);
-		for (int j = 0; j < width; j++)
-		{
+		j = -1;
+		while (++j < width)
 			visited[i][j] = false;
-		}
 	}
-	bool closed = true;
+	closed = true;
 	dfs(x, y, width, height, map, &closed, visited);
-
-	for (int i = 0; i < height; i++)
+	i = -1;
+	while (++i < height)
 		free(visited[i]);
 	free(visited);
-	return closed;
+	return (closed);
 }
--- src/parser/check_2.c
+++ src/parser/check_2.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   validator.c                                        :+:      :+:    :+:   */
+/*   check_2.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bmoll-pe <bmoll-pe@student.42.fr>          +#+  +:+       +#+        */
+/*   By: framos-p <framos-p@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/19 19:02:19 by bmoll-pe          #+#    #+#             */
-/*   Updated: 2023/07/19 19:10:01 by bmoll-pe         ###   ########.fr       */
+/*   Created: 2023/11/30 12:07:13 by framos-p          #+#    #+#             */
+/*   Updated: 2023/11/30 12:12:24 by framos-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,25 @@
 #include "mlx.h"
 #include <fcntl.h>
 
-bool validate_map(char *path, t_cub *cub);
-char *get_int_array(char *line);
-int get_data_type(char *line);
-int add_texture(char *path, t_texture *textures, t_mlx *screen, int type);
-int color_parser(char *line);
-int check_map(t_map *map);
-int map_builder(char **int_map, int scale, t_map *map, t_player *player);
-int **resize_matrix(int **matrix, int *width);
-int **get_image_matrix(char *data, int width, int height);
-t_texture *get_texture(t_texture *textures, int type);
-bool valid_map_from_player(int x, int y, char **map, int max_x, int max_y);
+bool		validate_map(char *path, t_cub *cub);
+char		*get_int_array(char *line);
+int			get_data_type(char *line);
+int			add_texture(char *path, t_texture *textures, t_mlx *screen,
+				int type);
+int			color_parser(char *line);
+int			check_map(t_map *map);
+int			map_builder(char **int_map, int scale, t_map *map,
+				t_player *player);
+int			**resize_matrix(int **matrix, int *width);
+int			**get_image_matrix(char *data, int width, int height);
+t_texture	*get_texture(t_texture *textures, int type);
+bool		valid_map_from_player(int x, int y, char **map, int max_x,
+				int max_y);
 
-char *get_int_array(char *line)
+char	*get_int_array(char *line)
 {
-	char *char_line;
-	int num_col;
-
-	//char_line = ft_calloc(sizeof(char), ft_strlen(line) + 1);
+	char	*char_line;
+	int		num_col;
 
 	num_col = 0;
 	while (line[num_col])
@@ -48,17 +49,22 @@ char *get_int_array(char *line)
 	return (line);
 }
 
-int get_data_type(char *line)
+int	get_data_type(char *line)
 {
-	if (line[0] && line[0] == 'N' && line[1] && line[1] == 'O' && line[2] && line[2] == ' ')
+	if (line[0] && line[0] == 'N' && line[1] && line[1] == 'O' && line[2]
+		&& line[2] == ' ')
 		return (NO);
-	if (line[0] && line[0] == 'S' && line[1] && line[1] == 'O' && line[2] && line[2] == ' ')
+	if (line[0] && line[0] == 'S' && line[1] && line[1] == 'O' && line[2]
+		&& line[2] == ' ')
 		return (SO);
-	if (line[0] && line[0] == 'W' && line[1] && line[1] == 'E' && line[2] && line[2] == ' ')
+	if (line[0] && line[0] == 'W' && line[1] && line[1] == 'E' && line[2]
+		&& line[2] == ' ')
 		return (WE);
-	if (line[0] && line[0] == 'E' && line[1] && line[1] == 'A' && line[2] && line[2] == ' ')
+	if (line[0] && line[0] == 'E' && line[1] && line[1] == 'A' && line[2]
+		&& line[2] == ' ')
 		return (EA);
-	if (line[0] && line[0] == 'S' && line[1] && line[1] == 'C' && line[2] && line[2] == ' ')
+	if (line[0] && line[0] == 'S' && line[1] && line[1] == 'C' && line[2]
+		&& line[2] == ' ')
 		return (SC);
 	if (line[0] && line[0] == 'C' && line[1] && line[1] == ' ')
 		return (C);
@@ -67,16 +73,17 @@ int get_data_type(char *line)
 	return (0);
 }
 
-int add_texture(char *path, t_texture *textures, t_mlx *screen, int type)
+int	add_texture(char *path, t_texture *textures, t_mlx *screen, int type)
 {
-	char *str_trimed;
+	char	*str_trimed;
 
 	while (textures->valid == true)
 		textures++;
 	str_trimed = ft_str_trim(path + 3);
 	textures->path = ft_substr(str_trimed, 0, ft_strlen(path) - 1);
 	free(str_trimed);
-	textures->img.ptr = mlx_xpm_file_to_image(screen->handler, textures->path, &textures->width, &textures->height);
+	textures->img.ptr = mlx_xpm_file_to_image(screen->handler, textures->path,
+			&textures->width, &textures->height);
 	if (textures->img.ptr == NULL)
 		return (EXIT_FAILURE);
 	textures->img.addr = mlx_get_data_addr(textures->img.ptr, NULL, NULL, NULL);
@@ -84,19 +91,19 @@ int add_texture(char *path, t_texture *textures, t_mlx *screen, int type)
 		return (EXIT_FAILURE);
 	textures->valid = true;
 	textures->type = type;
-	textures->img.matrix = resize_matrix(get_image_matrix(textures->img.addr, textures->width, textures->height), &textures->width);
+	textures->img.matrix = resize_matrix(get_image_matrix(textures->img.addr,
+				textures->width, textures->height), &textures->width);
 	textures->img.width = textures->width;
 	textures->img.height = textures->height;
 	return (EXIT_SUCCESS);
-	// Mirar si hace falta realloc
 }
 
-int color_parser(char *line)
+int	color_parser(char *line)
 {
-	char *trim_line;
-	char **splitted;
-	int color[3];
-	int hex;
+	char	*trim_line;
+	char	**splitted;
+	int		color[3];
+	int		hex;
 
 	trim_line = ft_str_trim(line + 1);
 	splitted = ft_split(trim_line, ',');
@@ -112,9 +119,8 @@ int color_parser(char *line)
 	return (hex);
 }
 
-int check_map(t_map *map)
+int	check_map(t_map *map)
 {
-
 	if (!get_texture(map->textures, NO))
 		return (EXIT_FAILURE);
 	if (!get_texture(map->textures, SO))
